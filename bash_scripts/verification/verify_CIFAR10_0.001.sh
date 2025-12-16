cb6353c6f43239e8114f01bffd63b64a6e564c8b
Duc Minh <106790159+ld-minh4354@users.noreply.github.com>
2025-12-15 22:11:52 -0500

    add verification script for cifar10

--- bash_scripts/verification/verify_CIFAR10_0.001.sh
+++ bash_scripts/verification/verify_CIFAR10_0.001.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=verify_MNIST_0.004
+#SBATCH --job-name=verify_CIFAR10_0.001
 #SBATCH --gpus=nvidia_h100_80gb_hbm3_2g.20gb:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=61G
 #SBATCH --time=3:00:00
 #SBATCH --array=0-449
-#SBATCH --output=logs/MNIST_0.004_%a.out
+#SBATCH --output=logs/CIFAR10_0.001_%a.out
 
 module load StdEnv/2023
 module load python/3.11
@@ -13,14 +13,14 @@ source $HOME/eml-verification/.venv_abc/bin/activate
 
 export OMP_NUM_THREADS=1
 
-EPSILON=0.004
+EPSILON=0.001
 
 for (( X=0; X<20; X++ )); do
     ID=$((SLURM_ARRAY_TASK_ID * 20 + X))
-    LOGFILE="logs_verification/MNIST_${EPSILON}_${ID}.out"
+    LOGFILE="logs_verification/CIFAR10_${EPSILON}_${ID}.out"
 
     {
-        srun python code/property_gen/generate_property_MNIST.py \
+        srun python code/property_gen/generate_property_CIFAR10.py \
         --epsilon $EPSILON --index $ID --job $SLURM_ARRAY_TASK_ID
 
         start_time=$(date +%s)
