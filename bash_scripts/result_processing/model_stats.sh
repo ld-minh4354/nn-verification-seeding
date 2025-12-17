b62f099c069aa3c4e8d25bf63301c8f143480013
Duc Minh <106790159+ld-minh4354@users.noreply.github.com>
2025-12-16 23:06:47 -0500

    update model stats script

--- bash_scripts/result_processing/model_stats.sh
+++ bash_scripts/result_processing/model_stats.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=model_stats_MNIST
+#SBATCH --job-name=model_stats
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=3G
-#SBATCH --time=0:20:00
-#SBATCH --output=logs/model_stats_MNIST.out
+#SBATCH --time=1:00:00
+#SBATCH --output=logs/model_stats.out
 
 module load StdEnv/2023
 module load python/3.11
@@ -14,4 +14,4 @@ pip install --no-index --upgrade pip
 
 pip install --no-index -r $HOME/requirements_main.txt
 
-srun python code/result_processing/model_stats_MNIST.py
+srun python code/result_processing/model_stats.py
