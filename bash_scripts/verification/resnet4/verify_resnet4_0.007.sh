9fc8771edcdbd9f01618490b297e2f83da69cf4e
Duc Minh <106790159+ld-minh4354@users.noreply.github.com>
2025-12-18 00:11:05 -0500

    update verification scripts

--- bash_scripts/verification/resnet4/verify_resnet4_0.007.sh
+++ bash_scripts/verification/resnet4/verify_resnet4_0.007.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=verify_MNIST_0.007
+#SBATCH --job-name=verify_resnet4_0.007
 #SBATCH --gpus=nvidia_h100_80gb_hbm3_2g.20gb:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=61G
 #SBATCH --time=3:00:00
 #SBATCH --array=0-449
-#SBATCH --output=logs/MNIST_0.007_%a.out
+#SBATCH --output=logs/resnet4_0.007_%a.out
 
 module load StdEnv/2023
 module load python/3.11
@@ -17,16 +17,16 @@ EPSILON=0.007
 
 for (( X=0; X<20; X++ )); do
     ID=$((SLURM_ARRAY_TASK_ID * 20 + X))
-    LOGFILE="logs_verification/MNIST_${EPSILON}_${ID}.out"
+    LOGFILE="logs_verification/resnet4_${EPSILON}_${ID}.out"
 
     {
-        srun python code/property_gen/generate_property_MNIST.py \
-        --epsilon $EPSILON --index $ID --job $SLURM_ARRAY_TASK_ID
+        srun python code/property_gen/generate_property.py \
+        --model 0 --epsilon $EPSILON --index $ID --job $SLURM_ARRAY_TASK_ID
 
         start_time=$(date +%s)
 
         timeout 5m srun python $HOME/eml-verification/alpha-beta-CROWN/complete_verifier/abcrown.py \
-        --config $HOME/eml-verification/properties/current_${SLURM_ARRAY_TASK_ID}.yaml
+        --config $HOME/eml-verification/properties/current_${EPSILON}_${SLURM_ARRAY_TASK_ID}.yaml
 
         end_time=$(date +%s)
         elapsed=$((end_time - start_time))
--- bash_scripts/verification/resnet8/verify_resnet8_0.003.sh
+++ bash_scripts/verification/resnet8/verify_resnet8_0.003.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=verify_resnet4_0.008
+#SBATCH --job-name=verify_resnet8_0.003
 #SBATCH --gpus=nvidia_h100_80gb_hbm3_2g.20gb:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=61G
 #SBATCH --time=3:00:00
 #SBATCH --array=0-449
-#SBATCH --output=logs/resnet4_0.008_%a.out
+#SBATCH --output=logs/resnet8_0.003_%a.out
 
 module load StdEnv/2023
 module load python/3.11
@@ -13,15 +13,15 @@ source $HOME/eml-verification/.venv_abc/bin/activate
 
 export OMP_NUM_THREADS=1
 
-EPSILON=0.008
+EPSILON=0.003
 
 for (( X=0; X<20; X++ )); do
     ID=$((SLURM_ARRAY_TASK_ID * 20 + X))
-    LOGFILE="logs_verification/resnet4_${EPSILON}_${ID}.out"
+    LOGFILE="logs_verification/resnet8_${EPSILON}_${ID}.out"
 
     {
         srun python code/property_gen/generate_property.py \
-        --model 0 --epsilon $EPSILON --index $ID --job $SLURM_ARRAY_TASK_ID
+        --model 2 --epsilon $EPSILON --index $ID --job $SLURM_ARRAY_TASK_ID
 
         start_time=$(date +%s)
 
