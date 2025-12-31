f58a98034795e0a8b325d5ad737d9ad69ec16eeb
Duc Minh <106790159+ld-minh4354@users.noreply.github.com>
2025-12-30 22:39:29 -0500

    add jpl pruning

---
+++ bash_scripts/JPL_models/prune_model.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=JPL_prune_model
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=3G
+#SBATCH --time=01:00:00
+#SBATCH --array=0-79
+#SBATCH --output=logs_training/JPL_prune_model_%a.out
+
+module load StdEnv/2023
+module load python/3.11
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index -r $HOME/requirements_main.txt
+
+PRUNE_VALUES=(10 20 30 40 50 60 70 80)
+SEED_VALUES=(10 20 30 40 50 60 70 80 90 100)
+
+TASK_ID=${SLURM_ARRAY_TASK_ID}
+
+PRUNE=${PRUNE_VALUES[$((TASK_ID / 10))]}
+SEED=${SEED_VALUES[$((TASK_ID % 10))]}
+
+echo "Prune JPL model with seed=$SEED, prune ratio=$PRUNE %"
+
+srun python code/JPL/prune_model.py --seed $SEED --prune $PRUNE
---
+++ code/JPL/prune_model.py
@@ -0,0 +1,199 @@
+import os, sys
+import argparse
+import random
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.nn.utils import prune
+from torchvision import datasets, transforms
+
+from model_architecture import ResNet4
+
+
+
+class PruneJPL:
+    def __init__(self, seed, prune_rate):
+        self.add_project_folder_to_pythonpath()
+        self.device = torch.device("cuda")
+
+        self.seed = seed
+        self.set_seed(seed)
+
+        self.prune_rate = prune_rate
+
+
+    def add_project_folder_to_pythonpath(self):
+        project_path = os.path.abspath("")
+        if project_path not in sys.path:
+            sys.path.append(project_path)
+
+
+    def set_seed(self, seed):
+        torch.manual_seed(seed)
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+
+    def main(self):
+        self.load_data()
+        self.load_model()
+        self.set_hyperparameters()
+        self.training()
+        self.save_model()
+
+
+    def count_zero_weights(self):
+        nonzero = 0
+        total = 0
+        for _, param in self.model.named_parameters():
+            tensor = param.data
+            nz = torch.count_nonzero(tensor)
+            nonzero += nz
+            total += tensor.numel()
+
+        print(f"Non-zero weights: {nonzero}/{total}")
+
+
+    def load_data(self):
+        self.num_classes = 2
+
+        transform_train = transforms.Compose([
+            transforms.Grayscale(num_output_channels=1),
+            transforms.RandomRotation(10),
+            transforms.ToTensor(),
+            transforms.Normalize((0.3989,), (0.1828,))
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.Grayscale(num_output_channels=1),
+            transforms.ToTensor(),
+            transforms.Normalize((0.3989,), (0.1828,))
+        ])
+
+        train_dir = os.path.join("raw_datasets", "JPL_processed", "train")
+        test_dir = os.path.join("raw_datasets", "JPL_processed", "test")
+
+        train_dataset = datasets.ImageFolder(root=train_dir, transform=transform_train)
+        test_dataset = datasets.ImageFolder(root=test_dir, transform=transform_test)
+
+        self.train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+        self.test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    
+    def load_model(self):
+        self.model = ResNet4()
+        self.model = self.model.to(self.device)
+        
+        state_dict = torch.load(os.path.join("models", "JPL", "baseline",
+                                             f"JPL_baseline_{self.seed}.pth"), 
+                                map_location=self.device)
+        self.model.load_state_dict(state_dict)
+
+        self.count_zero_weights()
+
+        self.parameters_to_prune = []
+        for _, module in self.model.named_modules():
+            if isinstance(module, nn.Conv2d):
+                self.parameters_to_prune.append((module, 'weight'))
+
+        prune.global_unstructured(
+            self.parameters_to_prune,
+            pruning_method=prune.L1Unstructured,
+            amount=self.prune_rate,
+        )
+
+
+    def set_hyperparameters(self):
+        self.EPOCH = 100
+        self.LR = 1e-4
+        self.WEIGHT_DECAY = 1e-4
+        self.STEP_SIZE = 30
+        self.GAMMA = 0.3
+
+
+    def training(self):
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.LR, weight_decay=self.WEIGHT_DECAY)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=self.STEP_SIZE, gamma=self.GAMMA)
+
+        self.criterion = nn.CrossEntropyLoss()
+
+        print(f"Pruning JPL model with seed {self.seed} under pruning ratio {self.prune_rate}\n")
+
+        for epoch in range(self.EPOCH):
+            test_accuracy = self.train_loop(epoch)
+            if test_accuracy >= 0.84:
+                break
+
+        for module, _ in self.parameters_to_prune:
+            prune.remove(module, 'weight')
+
+        self.count_zero_weights()
+
+
+    def save_model(self):
+        os.makedirs(os.path.join("models", "JPL", f"prune{self.prune_rate}"), exist_ok=True)
+        torch.save(self.model.state_dict(), os.path.join("models", "JPL", f"prune{self.prune_rate}",
+                                                         f"JPL_prune{self.prune_rate}_{self.seed}.pth"))
+
+
+    def train_loop(self, epoch):
+        self.model.train()
+        total_loss = 0
+
+        for _, (inputs, targets) in enumerate(self.train_loader):
+            inputs, targets = inputs.to(self.device), targets.to(self.device)
+            outputs = self.model(inputs)
+
+            self.optimizer.zero_grad()
+
+            loss = self.criterion(outputs, targets)
+            loss.backward()
+            total_loss += loss.item()
+
+            self.optimizer.step()
+
+        total_loss = total_loss / len(self.train_loader)
+        test_loss, test_accuracy = self.test_loop()
+
+        self.scheduler.step()
+        print(f"Epoch [{epoch+1:3d}] | Train Loss: {total_loss:.4f} | Test Loss: {test_loss:.4f} | Test Accuracy: {test_accuracy:.4f}", flush=True)
+
+        return test_accuracy
+    
+
+    def test_loop(self):
+        self.model.eval()
+
+        loss = 0
+        accuracy = 0
+
+        with torch.no_grad():
+            for inputs, targets in self.test_loader:
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+                outputs = self.model(inputs)
+
+                loss += self.criterion(outputs, targets).item()
+
+                pred = outputs.argmax(dim=1, keepdim=True)
+                accuracy += pred.eq(targets.view_as(pred)).sum().item()
+
+        loss = loss / len(self.test_loader)
+        accuracy = accuracy / len(self.test_loader.dataset)
+
+        return loss, accuracy
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int)
+    parser.add_argument("--prune", type=int)
+    args = parser.parse_args()
+
+    pruning = PruneJPL(seed=args.seed, prune_rate=args.prune / 100)
+    pruning.main()
